61e6517a1e3c5d3a7780621d713fa235b3efdaf8
zfima <zzfima@gmail.com>
2019-12-09 06:38:56 +0200

    dev

--- QSharpApplicationBellTest/Driver.cs
+++ QSharpApplicationBellTest/Driver.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Microsoft.Quantum.Simulation.Core;
 using Microsoft.Quantum.Simulation.Simulators;
 
@@ -8,10 +9,15 @@ namespace Quantum.QSharpApplicationBellTest
     {
         static void Main(string[] args)
         {
+            Task<(long, long)> task = null;
             using (var qsim = new QuantumSimulator())
             {
-                
+                task =  BellTest.Run(qsim, 100, Result.One);
+                task.Wait();
             }
+
+            Console.WriteLine("Num of zeroes : {0}, num of ones : {1}", task.Result.Item1, task.Result.Item2);
+            Console.ReadLine();
         }
     }
 }
